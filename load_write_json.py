bba7d2a3e27209d22a1669d04647590c6692d9f9
VdbFelix <felix.vandenborre147@gmail.com>
2025-05-04 10:19:01 +0100

    Create load_write_json.py
    From Mattingly's NER YouTube playlist

---
+++ load_write_json.py
@@ -0,0 +1,8 @@
+def load_data(file):
+    with open(file, "r", encoding="utf-8") as f:
+        data = json.load(f)
+    return (data)
+
+def save_data(file, data):
+    with open (file, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4)

a023e987952b421f086e8841bdc7a70878141f13
VdbFelix <felix.vandenborre147@gmail.com>
2025-05-28 10:43:33 +0100

    Update labelling_training_text.py

--- labelling_training_text.py
+++ labelling_training_text.py
@@ -66,6 +66,8 @@ Foxe_labels = pd.concat([Foxe_PERSON, Foxe_PLACE, Foxe_NORP], ignore_index=True)
 Foxe_labels.drop_duplicates()
 
 patterns = [{"label": row["label"], "pattern": row["entity"]} for _, row in Foxe_labels.iterrows()]
+patterns.append({"label": "YEAR", "pattern": [{"TEXT": {"REGEX": r"\b\d{3,4}\b"}}]})
+#NB: cannot combine DF patterns list with manually-defined rule in one string, hence patterns.append()
 print(patterns[0])
 
 #save_json("NER_patterns.json", patterns)
@@ -83,13 +85,36 @@ for item in training_corpus:
 
 TRAIN_DATA = []
 
-for item in corpus:
-    doc = nlp(item)
-
+#careful not to repeat doc object
+for doc in corpus:
     entities= []
     for ent in doc.ents:
         entities.append([ent.start_char, ent.end_char, ent.label_])
-    TRAIN_DATA.append([item, {"entities": entities}])
+    TRAIN_DATA.append([doc.text, {"entities": entities}])
 
-#verdergaan - DATUM pattern en model saven
+#now to train and save the model
+import srsly
+import typer
+import warnings
+from pathlib import Path
+from tqdm import tqdm 
 
+from spacy.tokens import DocBin
+#mattingly function
+def create_training_data(TRAIN_DATA):
+    db = DocBin()
+    for text, annot in tqdm(TRAIN_DATA):
+        doc = nlp.make_doc(text)
+        ents = []
+        for start, end, label in annot["entities"]:
+            span = doc.char_span(start, end, label=label)
+            if span is None:
+                print("SKipping entity")
+            else:
+                ents.append(span)
+        doc.ents = ents
+        db.add(doc)
+    return (db)
+
+db = create_training_data(TRAIN_DATA=TRAIN_DATA)
+db.to_disk("foxe_ner_model.spacy")
